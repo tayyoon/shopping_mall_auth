0d93fe4a217fae97d7f7267cef0c3823e9963f69
tay yoon <dylanyoon92@gmail.com>
2022-10-28 01:05:28 +0900

    sequelize 적용중

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const User = require('./models/user');
+const Goods = require('./models/goods');
+const Cart = require('./models/cart');
 const authMiddleware = require('./middlewares/auth-middleware');
 
 mongoose.connect('mongodb://localhost/shopping-demo', {
@@ -16,7 +18,6 @@ const app = express();
 const router = express.Router();
 
 // 회원가입
-
 const PostUsersSchema = Joi.object({
   nickname: Joi.string().required(),
   eamil: Joi.string().email().required(),
@@ -91,6 +92,125 @@ router.get('/users/me', authMiddleware, async (req, res, next) => {
   res.send({
     user: { nicename: user.nickname, email: user.email },
   });
+});
+
+/**
+ * 내가 가진 장바구니 목록을 전부 불러온다.
+ */
+router.get('/goods/cart', authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+
+  const cart = await Cart.find({
+    userId,
+  }).exec();
+
+  const goodsIds = cart.map((c) => c.goodsId);
+
+  // 루프 줄이기 위해 Mapping 가능한 객체로 만든것
+  const goodsKeyById = await Goods.find({
+    where: {
+      goodsId: goodsIds,
+    },
+  })
+    .exec()
+    .then((goods) =>
+      goods.reduce(
+        (prev, g) => ({
+          ...prev,
+          [g.goodsId]: g,
+        }),
+        {},
+      ),
+    );
+
+  res.send({
+    cart: cart.map((c) => ({
+      quantity: c.quantity,
+      goods: goodsKeyById[c.goodsId],
+    })),
+  });
+});
+
+/**
+ * 장바구니에 상품 담기.
+ * 장바구니에 상품이 이미 담겨있으면 갯수만 수정한다.
+ */
+router.put('/goods/:goodsId/cart', authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { goodsId } = req.params;
+  const { quantity } = req.body;
+
+  const existsCart = await Cart.findOne({
+    userId,
+    goodsId,
+  }).exec();
+
+  if (existsCart) {
+    existsCart.quantity = quantity;
+    await existsCart.save();
+  } else {
+    const cart = new Cart({
+      userId,
+      goodsId,
+      quantity,
+    });
+    await cart.save();
+  }
+
+  // NOTE: 성공했을때 딱히 정해진 응답 값이 없다.
+  res.send({});
+});
+
+/**
+ * 장바구니 항목 삭제
+ */
+router.delete('/goods/:goodsId/cart', authMiddleware, async (req, res) => {
+  const { userId } = res.locals.user;
+  const { goodsId } = req.params;
+
+  const existsCart = await Cart.findOne({
+    userId,
+    goodsId,
+  }).exec();
+
+  // 있든 말든 신경 안쓴다. 그냥 있으면 지운다.
+  if (existsCart) {
+    await existsCart.delete().exec();
+  }
+
+  // NOTE: 성공했을때 딱히 정해진 응답 값이 없다.
+  res.send({});
+});
+
+/**
+ * 모든 상품 가져오기
+ * 상품도 몇개 없는 우리에겐 페이지네이션은 사치다.
+ * @example
+ * /api/goods
+ * /api/goods?category=drink
+ * /api/goods?category=drink2
+ */
+router.get('/goods', authMiddleware, async (req, res) => {
+  const { category } = req.query;
+  const goods = await Goods.find(category ? { category } : undefined)
+    .sort('-date')
+    .exec();
+
+  res.send({ goods });
+});
+
+/**
+ * 상품 하나만 가져오기
+ */
+router.get('/goods/:goodsId', authMiddleware, async (req, res) => {
+  const { goodsId } = req.params;
+  const goods = await Goods.findById(goodsId).exec();
+
+  if (!goods) {
+    res.status(404).send({});
+  } else {
+    res.send({ goods });
+  }
 });
 
 app.use('/api', express.urlencoded({ extended: false }), router);
