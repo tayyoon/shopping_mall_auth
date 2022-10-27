241b742cbaf9616755307fd5a9bdcb4189b20b24
tay yoon <dylanyoon92@gmail.com>
2022-10-27 23:53:19 +0900

    validation

--- app.js
+++ app.js
@@ -16,35 +16,56 @@ const app = express();
 const router = express.Router();
 
 // 회원가입
+
+const PostUsersSchema = Joi.object({
+  nickname: Joi.string().required(),
+  eamil: Joi.string().email().required(),
+  password: Joi.string().required(),
+  confirmPassword: Joi.string().required(),
+});
+
 router.post('/users', async (req, res, next) => {
-  const { nickname, email, password, confirmPassword } = req.body;
+  try {
+    const { nickname, email, password, confirmPassword } =
+      await PostUsersSchema.validateAsync(req.body);
 
-  if (password !== confirmPassword) {
-    res
-      .status(400)
-      .json({ errorMessage: '비밀번호와 비밀번호 확인이 동일하지 않음' });
-    return;
+    if (password !== confirmPassword) {
+      res
+        .status(400)
+        .json({ errorMessage: '비밀번호와 비밀번호 확인이 동일하지 않음' });
+      return;
+    }
+
+    const checkUsers = await User.find({
+      $or: [{ email }, { nickname }], // $or 로 email도 nickname도 데이터베이스에 있는지 확인하는 것
+    });
+    if (checkUsers.length) {
+      // 유저정보가 겹치는것이 있는지 이야기 하는데, 어떤것이 있는건지 확실하게 알려주지 않기위해서 (해커위험)
+      res
+        .status(400)
+        .json({ errorMessage: '이미 가입된 이메일 또는 닉네임이 있습니다.' });
+      return;
+    }
+
+    const user = new User({ email, nickname, password });
+    user.save();
+    res.status(201).send({});
+  } catch (error) {
+    res.status(400).send({
+      errorMessage: '요청한 데이터형식이 올바르지 않습니다.',
+    });
   }
-
-  const checkUsers = await User.find({
-    $or: [{ email }, { nickname }], // $or 로 email도 nickname도 데이터베이스에 있는지 확인하는 것
-  });
-  if (checkUsers.length) {
-    // 유저정보가 겹치는것이 있는지 이야기 하는데, 어떤것이 있는건지 확실하게 알려주지 않기위해서 (해커위험)
-    res
-      .status(400)
-      .json({ errorMessage: '이미 가입된 이메일 또는 닉네임이 있습니다.' });
-    return;
-  }
-
-  const user = new User({ email, nickname, password });
-  user.save();
-  res.status(201).send({});
 });
 
 // 로그인
+
+const PostLoginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
 router.post('/auth', async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = await PostLoginSchema.validateAsync(req.body);
 
   const user = await User.findOne({ email, password }).exec(); // email과 password 둘다 일치하는게 있는지 확인하는것
 
@@ -61,7 +82,7 @@ router.post('/auth', async (req, res, next) => {
 
 router.get('/users/me', authMiddleware, async (req, res, next) => {
   const { user } = res.locals;
-  res.status(400).send({
+  res.send({
     user: { nicename: user.nickname, email: user.email },
   });
 });
